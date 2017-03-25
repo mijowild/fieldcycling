92dc5a1fff60d62a61dc6d4498f3fb8f07c59eb8
Mijo Wild <mijowild@gmail.com>
2017-03-25 19:31:19 +0100

    deleted pycache, added to gitignore. Deleted src, files are in main directory from now on

--- src/timing.py
+++
@@ -1,9 +0,0 @@
-import pstats
-p = pstats.Stats('cputimes')
-p.strip_dirs().sort_stats(-1).print_stats()
-
-
-print(p.sort_stats('time').print_stats(10))
-
-
-print(p.sort_stats('cumulative').print_stats(10))
