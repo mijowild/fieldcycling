39a194992728009a1af9c6e5b82751a54395596c
harrispilton <harris_pilton@gmx.de>
2017-04-05 00:02:44 +0200

    clean some comments

--- bokeh_throttle_fieldcycling.py
+++ bokeh_throttle_fieldcycling.py
@@ -5,7 +5,6 @@ import sys
 from datetime import datetime
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
 import re
 from bokeh.charts import Scatter, output_file, show
 from bokeh.sampledata.autompg import autompg as df
@@ -21,15 +20,14 @@ from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider
-#from bokeh.models.axes import DatetimeAxis
 from bokeh.server.server import Server
 import stelardatafile as sdf
 from utils import get_x_axis
 from scipy.optimize import leastsq
 
 #specify and import data file
-path=os.path.join(os.path.curdir,'more_data')
-polymer=sdf.StelarDataFile('100kHz-melting.sdf',path)
+path=os.path.join(os.path.curdir,'data')
+polymer=sdf.StelarDataFile('297K.sdf',path)
 polymer.sdfimport()
 nr_experiments = polymer.get_number_of_experiments()
 
@@ -44,7 +42,6 @@ columns=sorted(par_df.columns)
 discrete = [x for x in columns if (par_df[x].dtype == object or par_df[x].dtype == str)]
 continuous = [x for x in columns if x not in discrete]
 time = [x for x in continuous if x=='TIME']
-#continuous.remove('TIME')
 quantileable = [x for x in continuous if len(par_df[x].unique()) > 20]
 
 
@@ -66,16 +63,19 @@ def modify_doc(doc):
     except:
         dw=1
     temperature=parameters['TEMP']
+
     #calculate series of fid
     fid=pd.DataFrame(polymer.getdata(ie),index=np.linspace(dw,dw*bs*nblk,bs*nblk),
                      columns=['real', 'im'])/ns
     fid['magnitude']=( fid['real']**2 + fid['im']**2 )**0.5 # last two lines may represent a seperate method
-    #calculate the tau for the corresponding fid in each block
-    #calculate magnitization amplitudes from fid series, integrate from startpoint to endpoint
-    #seems working for the moment # TODO: more testing
+
+    # TODO: more testing on get_x_axis
     tau = get_x_axis(parameters, nblk)
+
+    #calculate magnetization:
+    #integrate fids from start- to endpoint to get the amplitude
     startpoint=int(0.05*bs)-1
-    endpoint=int(0.1*bs)
+    endpoint=int(0.1*bs) #TODO: make a range slider to get start- and endpoint interactively
     phi=np.zeros(nblk)
     for blk in range(nblk):
         start=startpoint + blk * bs
@@ -104,42 +104,17 @@ def modify_doc(doc):
         C0 = 0 # offset
         popt = fit_exp_linear(df.tau, df.phi_normalized, C0)
     elif fit_option == 2:
-        # needs prior knowledge for p0...
-        #popt, _ = curve_fit(model_func, np.array(df.tau), np.array(df.phi_normalized), p0=[1.0, 0.1**-1, 0.0])
+        # needs prior knowledge for p0...        
         p0=[1.0, 0.1**-1, 0.0]
         popt, _ = leastsq(fun, p0  , args=(np.array(df.tau),np.array(df.phi_normalized)) )
     df['fit_phi'] = model_func(df.tau, *popt)
 
     
 
-    # get a plot of two parameters:
-    # TODO write function which takes parameter name and returns list with values of all values found in the object
-    # start with zone, vs time. # TODO choose any parameter from the dropdown and think of nice visualization
-##    par_x_name = 'ZONE'
-##    par_y_name = 'TIME'
-##    # i know there is a pythonic oneliner for the next couple of lines forgive me with lambda: something i dont remember at the moment :)
-##    par_y=[]
-##    par_x=[]
-##    for i in range(nr_experiments):
-##        # 
-##        
-##        try:
-##            par_y.append(pd.to_datetime(polymer.getparameter(i)[par_y_name]))
-##        except:
-##            print('bla')
-##            par_y.append(polymer.getparameter(i)[par_y_name])
-##        try:
-##            par_x.append(pd.to_datetime(polymer.getparameter(i)[par_x_name]))
-##        except:
-##            print('bla')
-##            par_x.append(polymer.getparameter(i)[par_x_name])
-##    
-##    df_par = pd.DataFrame(data=np.c_[par_x, par_y], columns=[par_x_name, par_y_name])
-
+    
     # convert data to handle in bokeh
     source_fid = ColumnDataSource(data=ColumnDataSource.from_df(fid))
     source_df = ColumnDataSource(data=ColumnDataSource.from_df(df))
-##    source_par = ColumnDataSource(data=ColumnDataSource.from_df(df_par))
     
     # create and plot figures
     p1 = figure(plot_width=300, plot_height=300,
@@ -154,12 +129,6 @@ def modify_doc(doc):
     p2.circle_cross('tau', 'phi_normalized', source=source_df, color="navy")
     p2.line('tau', 'fit_phi', source=source_df, color="teal")
 
-
-##    p3 = figure(plot_width=300, plot_height=300,
-##                title='Parameter Plot',y_axis_type="datetime")
-##    p3.circle_cross(par_x_name, par_y_name, source=source_par, color="red")
-
-    
 
     # in the plot 4 use followingimpo
     SIZES = list(range(6, 22, 3)) # for some sizes
@@ -244,12 +213,6 @@ def modify_doc(doc):
         fid['magnitude']=( fid['real']**2 + fid['im']**2 )**0.5
         source_fid.data = ColumnDataSource.from_df(fid)
         
-        #
-        #
-        #
-        #
-        #
-        
         try:
             tau = get_x_axis(parameters, nblk)
             startpoint=int(0.05*bs)-1
@@ -278,7 +241,6 @@ def modify_doc(doc):
             elif fit_option == 2:
                 # needs prior knowledge for p0...
                 from scipy.optimize import leastsq
-                #popt, _ = curve_fit(model_func, np.array(df.tau), np.array(df.phi_normalized), p0=[1.0, 0.1**-1, 0.0])
                 p0=[1.0, 0.1**-1, 0.0]
                 popt, _ = leastsq(fun, p0  , args=(np.array(df.tau),np.array(df.phi_normalized)) )
             df['fit_phi'] = model_func(df.tau, *popt)
@@ -299,9 +261,8 @@ def modify_doc(doc):
             elif fit_option == 2:
                 # needs prior knowledge for p0...
                 from scipy.optimize import leastsq
-                #popt, _ = curve_fit(model_func, np.array(df.tau), np.array(df.phi_normalized), p0=[1.0, 0.1**-1, 0.0])
                 p0=[1.0, 0.1**-1, 0.0]
-                popt, _ = leastsq(fun, p0  , args=(np.array(df.tau),np.array(df.phi_normalized)) )
+                popt, _ = leastsq(fun, p0, args=(np.array(df.tau),np.array(df.phi_normalized)) )
             df['fit_phi'] = model_func(df.tau, *popt)
             source_df.data = ColumnDataSource.from_df(df)
         except KeyError:
@@ -322,20 +283,8 @@ def modify_doc(doc):
         source.data = { value: [cb_obj.value] }
     """)#unfortunately this customjs is needed to throttle the callback in current version of bokeh
 
-    # choose the parameters for plot p3 in a dropdown menu
-    #parameters given may vary in one file, merge the parameters
-##    merge_par=parameters
-##    for i in range(1,nr_experiments):
-##        add_par=polymer.getparameter(i) #read the parameters
-##        merge_par.update(add_par) #and merge them into one dictionary
-##    
-##    menu = [(key, "None") for key in merge_par.keys()] 
-##    dropdown_x_par = Dropdown(label="parameter x-axis", menu=menu)    
-##    dropdown_y_par = Dropdown(label="parameter y-axis", menu=menu)
-
-
     
-    # try to add some select boxes for p4
+    # add select boxes for p4
     x = Select(title='X-Axis', value='ZONE', options=columns)
     x.on_change('value', update)
 
@@ -351,9 +300,7 @@ def modify_doc(doc):
 
     controls_p4 = widgetbox([x,y,color,size], width=150)
     layout_p4 = row(controls_p4,plot_par())
-    doc.add_root(column(slider, p1, p2,
-                        #p3, dropdown_x_par, dropdown_y_par,
-                        ))
+    doc.add_root(column(slider, p1, p2))
     doc.add_root(layout_p4)
     doc.add_root(source) # i need to add the source for some reason...
 
